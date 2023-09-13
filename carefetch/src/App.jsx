94e4394c1e3e9628d1c38387662f687f67e3b5e0
aalimaslam-mm <aalim.marvelminds@gmail.com>
2023-09-13 19:35:19 +0530

    Login Page

--- carefetch/src/App.jsx
+++ carefetch/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <Routes>
-      <Route path="/register" element={<Signup />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 export default App;
